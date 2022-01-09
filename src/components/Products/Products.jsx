3c8a5126437ea9a0572974590add7776b4f4fc52
Taniuop <taniuop@gmail.com>
2022-01-09 11:32:42 +0100

    reset filters, sort by name

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -6,6 +6,8 @@ const Products = () => {
 
   const [productList, setAllProducts] = useState([])
   const [productSearch, setProductSearch] = useState("");
+  const [inputValue, setinputValue] = useState();
+
 
   useEffect(() => {
     async function fetchProducts() {
@@ -13,48 +15,65 @@ const Products = () => {
         if (productSearch) {
           axios.get(`http://localhost:3000/api/products/?name=${productSearch}`).then((allProducts) => {
             setAllProducts(allProducts.data)
-        }) 
-      }else {
+          })
+        } else {
           axios.get('http://localhost:3000/api/products/').then((allProducts) => {
             setAllProducts(allProducts.data)
           })
         }
-      }catch (e) {
-    setAllProducts([])
-    console.log("We had an error loading the data")
+      } catch (e) {
+        setAllProducts([])
+        console.log("We had an error loading the data")
+      }
+    } fetchProducts()
+  }, [productSearch])
+
+  const paintProducts = () => {
+    return productList.map((product, i) => <Card productsInfo={product} key={i} />)
   }
-  } fetchProducts()
-},[productSearch])
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const searchedProduct = event.target.searchProduct.value
+    setProductSearch(searchedProduct)
+  }
 
-const paintProducts = () => {
-  return productList.map((product, i) => <Card productsInfo={product} key={i} />)
-}
+  const resetFilters = (e) => {
+    setProductSearch("")
+    axios.get('http://localhost:3000/api/products/').then((allProducts) => {
+      setAllProducts(allProducts.data)
+    })
+    setinputValue(() => "")
+  }
 
-const handleSubmit = (event) => {
-  event.preventDefault();
-  const searchedProduct = event.target.searchProduct.value
-  setProductSearch(searchedProduct)
-}
+  const sortByName = () => {
+    var sortByNameA = productList.sort((a, b) =>
+      a.name.localeCompare(b.name)
+    )
+    setAllProducts(sortByNameA)
+    console.log(sortByNameA)
+    paintProducts(sortByNameA)
+  }
 
   return <div>
     <h3>Order by</h3>
-    <input type="button" value="name"/>
-    <input type="button" value="ratings"/>
-    <input type="button" value="price"/>
+    <input type="button" value="name" onClick={sortByName} />
+    <input type="button" value="ratings" />
+    <input type="button" value="price" />
 
     <p>Search products:</p>
     <form onSubmit={handleSubmit}>
-      <input type="text" name="searchProduct" id="searchProduct"/>
+      <input type="text" name="searchProduct" id="searchProduct" value={inputValue} />
       <button>Search</button>
     </form>
 
+    <button onClick={resetFilters}>Reset filters</button>
+
     <h1>Products</h1>
-
-    <>
-    {paintProducts()}
-    </>
-
+    {productList === "" ?
+      <p>No products found. Try another search.</p> :
+      <p> {productSearch} </p>}
+    <> {paintProducts()} </>
   </div>;
 };
 
